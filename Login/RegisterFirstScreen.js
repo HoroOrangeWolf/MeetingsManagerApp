1edc14d67e07bd733d9f81bdd3faa3745f8d2863
HoroOrangeWolf <81477769+HoroOrangeWolf@users.noreply.github.com>
2021-10-30 23:19:06 +0200

    some fixes

--- Login/RegisterFirstScreen.js
+++ Login/RegisterFirstScreen.js
@@ -15,13 +15,19 @@ export default function RegisterFirstScreen({navigation}) {
 
     return (
         <Box style={globalStyles.logRegScreen}>
+
             <Text style={globalStyles.fatText}>Login</Text>
+
             <Input style={globalStyles.Input}  placeholder="Login"/>
+            
             <Input style={globalStyles.Input}  placeholder="Email"/>
+
             <Button style={globalStyles.button} onPress={()=>navigation.navigate("ConfirmPassword")}>
+                
                 <Text style={globalStyles.buttonText}>
                     Next
                 </Text>
+
             </Button>
         </Box>
     );
