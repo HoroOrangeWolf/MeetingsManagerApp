b98bac10c6aee309f74f21102b0df93a86be8819
NoscanX <j.michalski7@gmail.com>
2021-12-29 13:59:01 +0100

    List moved to separate .js file

---
+++ Home/List.js
@@ -0,0 +1,64 @@
+import React from "react"
+
+import { Box,FlatList,HStack,VStack,Text,Spacer} from "native-base"
+
+import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
+
+export default function List({data}) {
+    return (
+        <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <Pressable onPress={()=>{console.log("ta")}} onLongPress = {()=>longPress(item)}>
+                <Box
+                borderBottomWidth="4"
+                _dark={{
+                  borderColor: "gray.600",
+                }}
+                borderColor="coolGray.200"
+                pl="4"
+                pr="5"
+                py="2"
+              >
+                <HStack space={1} justifyContent="space-between">
+
+                  <VStack>
+                    <Text
+                      _dark={{
+                        color: "warmGray.50",
+                      }}
+                      color="coolGray.800"
+                      bold
+                    >
+                      {`Nazwa spotkania: ${item.name}`}
+                    </Text>
+
+                    <Text
+                      color="coolGray.600"
+                      _dark={{
+                        color: "warmGray.200",
+                      }}
+                    >
+                      {`Opis: ${item.description}`}
+                    </Text>
+                  </VStack>
+
+                  <Spacer />
+                  <Text
+                    fontSize="xs"
+                    _dark={{
+                      color: "warmGray.50",
+                    }}
+                    color="coolGray.800"
+                    alignSelf="flex-start"
+                  >
+                    {new Date(item.timeDate).toUTCString()}
+                  </Text>
+                </HStack>
+              </Box>
+          </Pressable>
+        )}
+        keyExtractor={(item) => item.id}
+      />
+    )
+}
