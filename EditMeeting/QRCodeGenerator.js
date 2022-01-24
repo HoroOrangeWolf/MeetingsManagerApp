e1fd79fb2f53edd4f6c53e9b7dbb5113d053ba34
HoroOrangeWolf <81477769+HoroOrangeWolf@users.noreply.github.com>
2022-01-24 14:57:41 +0100

    fixed qr code

--- EditMeeting/QRCodeGenerator.js
+++ EditMeeting/QRCodeGenerator.js
@@ -7,11 +7,13 @@ export default function QRCodeGenerator({navigation, route}) {
 
     const {globalStyles: {meetingDetailsScreen, fatText, button, buttonText}} = useGlobalContext();
 
+    const { value } = route.params;
+
     return (
         <Box style={meetingDetailsScreen}>
             <Center flex={1} px="3">
                 <Text style={{...fatText, fontSize: 20, marginBottom: 40}}>Kod QR Twojego spotkania</Text>
-                <QRCode value="http://awesome.link.qr" size={200}/>
+                <QRCode value={value} size={200}/>
                 <Button style={{...button, width: "100%", marginTop: 40}} onPress={()=>navigation.goBack()}>
                     <Text style={buttonText}>
                             Powrót
